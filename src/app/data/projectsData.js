821b326b0bddb4dbad595c40f864279b7fd5b745
Yashu333 <saichintakayala4@gmail.com>
2024-10-05 14:57:47 +0100

    Trying to fix images

--- src/app/data/projectsData.js
+++ src/app/data/projectsData.js
@@ -5,28 +5,28 @@ const projects = [
         title: "UniCLubs", 
         description: " Developed an android application for university students enabling them to create, join, and manage clubs enhancing campus interactions.", 
         gitHubLink: "https://github.com/Yashu333/UniClubs",
-        imagePath: "https://drive.google.com/uc?export=view&id=1tyqoD04FymAj8hR86ypz6b4_yodHhSlS",
+        imagePath: "/images/AppLogo.png",
         livePreviewLink: ""
     },
     {
         title: "Frozen Lake AI", 
         description: "Created an GUI frozen lake environment with agent cross frozen lake with hurdles using various algorithms in Artificial Intelligence.", 
         gitHubLink: "https://github.com/Yashu333/Frozen_Lake_AI" ,
-        imagePath: "https://drive.google.com/uc?export=view&id=1I4D78Rt0ej6aGnA9n5hDmpLtRdKj6SLb",
+        imagePath: "/images/AIImage.gif",
         livePreviewLink: ""
     },
     {
         title: "Life Expectancy Prediction", 
         description: "Identified Life expectancy trends for various countries , predicted the future life expectancy rates. Analyzed how better life style leads to higher expectancy rates.", 
         gitHubLink: "https://github.com/Yashu333/Life_Expectancy_ML" ,
-        imagePath: "https://drive.google.com/uc?export=view&id=1vtubsYBVxyCeSXvcGEL7Khq-0HImyzvr",
+        imagePath: "/images/MLImage.png",
         livePreviewLink: ""
     },
     {
         title: "Sports Consortium", 
         description: "Created a console-based GUI application for sports consortium with many features.", 
         gitHubLink: "https://github.com/Yashu333/Ashers_Sport_Consortium_OOP" ,
-        imagePath: "https://drive.google.com/uc?export=view&id=1kJ1gVR2GKe_Akqr5qgm3Gu0m1RhSevkJ", 
+        imagePath: "/images/OOPImage.png", 
         livePreviewLink: ""
     }
 ]
